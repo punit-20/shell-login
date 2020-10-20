029622f90e9f43229b2dda968200cc10eafc49f0
Punit-art <68048936+Punit-art@users.noreply.github.com>
2020-10-20 22:59:14 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ pip install lolcat
 
 echo ""
 echo -e "\e[1mExit termux to apply changes\e[0m"
-echo -en "\e[93m Have you subscribe our channel (y/n)? \e[m "
+echo -en "\e[93m Do you Want Stylish font (y/n) y=yes ,n=no? \e[m "
 read answer
 
 if [ "$answer" != "${answer#[Yy]}" ] ;then
