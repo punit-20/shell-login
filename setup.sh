8d4981b3e3dd8f4065862af54a04117a0990c047
Punit-art <68048936+Punit-art@users.noreply.github.com>
2020-10-20 22:57:59 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,4 +15,13 @@ pip install lolcat
 
 echo ""
 echo -e "\e[1mExit termux to apply changes\e[0m"
-exit
+echo -en "\e[93m Have you subscribe our channel (y/n)? \e[m "
+read answer
+
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+tput cup 17 21
+    echo -e "\e[42m Thankyou for subscribing our channel \e[m"
+else
+tput cup 17 23
+    echo -e "\e[41m Please subsrcibe our channel \e[m"
+fi
